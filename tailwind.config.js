f32f1b5dc62280e7494c52e5d59b6de7db1a082d
Pivok <pivoc@protonmail.com>
2025-11-23 19:58:38 +0100

    Clean up

--- tailwind.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    './**/*.{html,js,jsx,ts,tsx}', // Adjust the paths based on your project's structure
-  ],
-  plugins: [],
-}
